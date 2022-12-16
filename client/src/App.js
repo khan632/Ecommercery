9e7d5d04ab25038a4028b12ee5d6ed83b0f0a598
md areeb uddin <mdareeb.uddin@truelancer.com>
2022-12-16 14:15:21 +0530

    Create database and navbar

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+
+import Home from './scenes/home/Home'; 
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar'
+
+const ScrollToTop = () => {
+  const {pathName} = useLocation();
+  // console.log(pathName);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathName])
+
+  return null;
+}
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
